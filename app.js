83046b152b5a16c7684560f87133bf592285393e
jakechanan <jake_chanan@brown.edu>
2020-10-27 18:35:26 -0500

    done

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 var corsApiUrl = "https://cors-anywhere.herokuapp.com/";
 // TODO: REPLACE YOUR TOKEN
-var apiToken = "?token=YOUR_TOKEN_HERE";
+var apiToken = "?token=vIhThpOoHPRSNSfg30iZ8VUkTJ_yNrIwLuwkrn0MqC4";
+
+var caps = false;
+var full = false;
 
 // CORS stands for "cross origin resource sharing" -- you'll be making http requests in order
 // DON'T CHANGE THIS: fetches the data from the API endpoint
@@ -15,7 +18,7 @@ const doCORSRequest = (options) => {
 const corsPromise = () =>
   new Promise((resolve, reject) => {
     const request = doCORSRequest({
-      url: "https://trefle.io/api/v1/plants" + apiToken,
+      url: "https://trefle.io/api/v1/families" + apiToken,
     });
     resolve(request);
   });
@@ -23,9 +26,58 @@ const corsPromise = () =>
 // THIS IS SOME SAMPLE CODE FOR HOW TO USE PROMISES -- feel free to adapt this into a function!
 corsPromise().then(
   (request) =>
-    (request.onload = request.onerror = function () {
-      // TODO: ADD FUNCTION, ETC. FOR WHATEVER YOU WANT TO DO ONCE THE DATA IS RECEIVED
-    })
+  (request.onload = request.onerror = function() {
+    // TODO: ADD FUNCTION, ETC. FOR WHATEVER YOU WANT TO DO ONCE THE DATA IS RECEIVED
+    families = JSON.parse(request.response).data
+    makeTable(families)
+    document.getElementById('caps').onclick = function(event) {
+      caps = !caps;
+      makeTable(families)
+    }
+    document.getElementById('full').onclick = function(event) {
+      full = !full;
+      makeTable(families)
+    }
+  })
 );
+
+function tableRow(family) {
+  return `
+  <tr>
+    <td>${family.common_name ? family.common_name : ""}</td>
+    <td>${family.name}</td>
+    <td>${(family.division_order) ? family.division_order.name : ""}</td>
+  </tr>
+  `
+}
+
+function makeTable(families) {
+  if (caps) {
+    families = families.map(function(family) {
+      return {
+        common_name: family.common_name,
+        name: family.name.toUpperCase(),
+        division_order: family.division_order,
+      }
+    })
+  }
+
+  if (full) {
+    families = families.filter(function(family) {
+      return family.common_name && family.name && family.division_order;
+    })
+  }
+
+  var table = document.getElementById('table');
+  var old_tbody = table.getElementsByTagName('tbody')[0];
+  var new_tbody = document.createElement('tbody');
+
+  families.forEach((family, _) => {
+    var row = new_tbody.insertRow(new_tbody.rows.length);
+    row.innerHTML = tableRow(family)
+  });
+
+  old_tbody.parentNode.replaceChild(new_tbody, old_tbody);
+}
 
 //// TODO: ADD WHATEVER FUN CONTENT YOU WANT ////
